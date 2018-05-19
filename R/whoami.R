af75b2c8c46e6d2d2000cd4f872e0aa37d0119ff
Janus Valberg-Madsen <janus@math.aau.dk>
2018-05-18 22:21:02 -0500

    Make whoami work under sourcing
    whoami now works either when running a script with Rscript or source.

--- R/whoami.R
+++ R/whoami.R
@@ -1,7 +1,10 @@
 #' Get name the executing script file
 #'
-#' Utility function that, when used with Rscript, detects the file
-#' running and returns a vector the basename without extension.
+#' Utility function that tries to detecs the file being run,
+#' either with Rscript or with `source`, and returns the
+#' basename without file extension.
+#' If the function is not called as a result of a file being
+#' run, it throws a warning and returns an empty string.
 #'
 #' @return String of basename.
 #'
@@ -9,12 +12,26 @@
 whoami <- function() {
 
   # Return nothing if running interactively
-  if (interactive()) return(NULL)
+  # if (interactive()) return(NULL)
 
   # Get command line arguments and find the filename
+  # or get it from the env if file is sourced
   cmd_args <- commandArgs(trailingOnly = FALSE)
   file_arg <- "--file="
-  path <- sub(file_arg, "", cmd_args[grep(file_arg, cmd_args)])
+  if (length(m <- grep(file_arg, cmd_args)) > 0L) {
+    path <- sub(file_arg, "", cmd_args[m])
+  } else {
+      path <- try(get("filename", envir = sys.frame(1L)),
+                  silent = TRUE)
+  }
+
+  # Return nothing if a file name could not be found
+  if (inherits(path, "try-error")) {
+    warning("Filename not detected. Did you run this function interactively?")
+    return(character(0L))
+  }
+
+  # Strip path and file extension
   name <- basename(path)
   name <- gsub("\\..+", "", name)
   return(name)
